e78e0d7640097aebf71eaa8020cd46ef746a2699
Muhammad Hassan <muhammadd.h.khaku@gmail.com>
2022-12-05 22:45:04 -0500

    WeatherPy.ipynb

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "ece73e5bb90021f06f20096222f90748"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "56a81c729e4147c5ad82a3b2cc24e937"
